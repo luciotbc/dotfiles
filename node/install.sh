b7bee3f199eaf321f7022623463d0fdfaa6462d0
Lucio Charallo <hi@lucio.app>
2019-07-01 23:35:47 -0300

    add dotfiles to node

---
+++ node/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck source=helpers.sh
+. ../helpers.sh
+
+echo "Add NodeJS keys..."
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+echo "Symling .default-npm-packages..."
+ln -sfT ~/.dotfiles/ruby/default-npm-packages ~/.default-npm-packages
+
+echo_done "NodeJS configuration!"
