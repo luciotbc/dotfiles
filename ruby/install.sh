52f2aec8c28d799039e225803b26c9a9782e568d
Lucio Charallo <hi@lucio.app>
2019-07-01 23:35:57 -0300

    add dotfiles to ruby

---
+++ ruby/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck source=helpers.sh
+. ../helpers.sh
+
+echo_info "Symling .default-gems..."
+ln -sfT ~/.dotfiles/ruby/default-gems ~/.default-gems
+
+echo_info "Symling .irbrc..."
+ln -sfT ~/.dotfiles/ruby/irbrc ~/.irbrc
+
+echo_done "Ruby configuration!"
