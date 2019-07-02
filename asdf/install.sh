a8f50b49d4cc13e29dcc40bfdf136c9d7678495a
Lucio Charallo <hi@lucio.app>
2019-07-01 23:32:43 -0300

    add configuraion to asdf

---
+++ asdf/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# shellcheck source=helpers.sh
+. ../helpers.sh
+
+echo_info "Cloning asdf..."
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+SAVED_DIR=$PWD
+
+cd ~/.asdf || exit
+
+git checkout "$(git describe --abbrev=0 --tags)"
+
+cd "$SAVED_DIR" || exit
+
+echo_info "Symling .asdfrc..."
+ln -sfT ~/.dotfiles/asdf/asdfrc ~/.asdfrc
+
+echo_info "Symling .tool-versions..."
+ln -sfT ~/.dotfiles/asdf/tool-versions ~/.tool-versions
+
+echo_info "Update asdf itself..."
+asdf update
+
+echo_info "Add asdf plugins..."
+asdf plugin-add elixir
+asdf plugin-add nodejs
+asdf plugin-add ruby
+
+echo_info "Update all plugins..."
+asdf plugin-update --all
+
+echo_done "asdf configuration!"
