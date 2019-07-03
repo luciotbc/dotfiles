93caee89f604bbd54ed509881f10d98f61cbf09a
Lucio Charallo <hi@lucio.app>
2019-07-03 20:13:47 -0300

    try fix zsh_highlight

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,10 +10,13 @@ sh install.sh --unattended
 echo_info "Installing zsh-autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
+echo_info "Installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
 echo_info "Symlink .zshrc..."
 ln -sfT "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
 
 echo_info "Changing shell..."
-chsh -s "$(command -v zsh)"
+sudo chsh -s $(which zsh)
 
 echo_done "ZSH configuration!"
