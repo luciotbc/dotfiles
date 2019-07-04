ff23c5c2dc8e44ba023cb58a60886d127d1f1673
Lucio Charallo <hi@lucio.app>
2019-07-03 23:25:26 -0300

    map fzf to crlt+p

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -17,27 +17,5 @@ plugins=(
 source $DOTFILES/zsh/plugins/syntax-highlighting.zsh
 [[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
 
-# ### zplug #####################################################################
-
-# # Check if zplug is installed
-# if [[ ! -d ~/.zplug ]]; then
-#   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-#   source ~/.zplug/init.zsh && zplug update --self
-# fi
-
-# source ~/.zplug/init.zsh
-
-# # Plugins List
-
-# # Install packages that have not been installed yet
-# if ! zplug check --verbose; then
-#   printf "Install? [y/N]: "
-#   if read -q; then
-#     echo
-#     zplug install
-#   else
-#     echo
-#   fi
-# fi
-
-# zplug load
+# Map fzf to CRLT+P
+bindkey '^P' fzf-file-widget
