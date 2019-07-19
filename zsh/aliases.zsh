b6c01aeadebe0b9f0e239e751a9646c5515c122c
Lucio Charallo <hi@lucio.app>
2019-07-18 21:44:57 -0300

    new alias sysupdpate

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,9 @@ alias v="nvim"
 alias vim="nvim"
 alias :q="exit"
 
+# System
+alias sysupdate="sudo pacman -Syu --needed --noconfirm"
+
 # Pipefy
 alias pdcup="docker-compose -f full-docker-compose.yml up web sidekiq"
 alias pdcr="docker-compose -f full-docker-compose.yml run"
