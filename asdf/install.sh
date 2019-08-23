12c23b31eb1dc2242874b3e6b70de19f39f58c77
Lucio Charallo <hi@lucio.app>
2019-08-23 16:55:41 -0300

    add erlang on asdf

--- asdf/install.sh
+++ asdf/install.sh
@@ -24,6 +24,7 @@ echo_info "Update asdf itself..."
 ~/.asdf/bin/asdf update
 
 echo_info "Add asdf plugins..."
+~/.asdf/bin/asdf plugin-add erlang
 ~/.asdf/bin/asdf plugin-add elixir
 ~/.asdf/bin/asdf plugin-add nodejs
 ~/.asdf/bin/asdf plugin-add ruby
@@ -31,9 +32,13 @@ echo_info "Add asdf plugins..."
 echo_info "Update all plugins..."
 ~/.asdf/bin/asdf plugin-update --all
 
+echo_info "asdf install erlang..."
+~/.asdf/bin/asdf install erlang 21.1.1
+~/.asdf/bin/asdf global erlang 21.1.1
+
 echo_info "asdf install elixir..."
-~/.asdf/bin/asdf install elixir 1.9.0
-~/.asdf/bin/asdf global elixir 1.9.0
+~/.asdf/bin/asdf install elixir 1.9.1
+~/.asdf/bin/asdf global elixir 1.9.1
 
 echo_info "asdf install nodejs..."
 ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
