eb4281ff72bd8ca955491013c28936a9863726f8
Lucio Charallo <hi@lucio.app>
2019-09-02 07:53:19 -0300

    add dev dependecies

--- packages.sh
+++ packages.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 export PKG=(
+  albert
   awesome-terminal-fonts 
+  base-devel
   curl
   diff-so-fancy
   fzf
+  gcc5
   htop
   lastpass-cli
+  libffi
+  libyaml
+  muparser
+  openssl
   otf-fira-code
   postgresql-libs
   powerline-fonts
+  sqlite3
+  terminator
   tmux
+  transmission-gtk
   unzip
+  vlc
   xclip
   yay
+  zlib
   zsh
-  transmission-gtk
-  vlc
-  # albert
-  # muparser
 )
 
 export AUR=(
+  balena-etcher
   chromium-widevine
   ctop
   google-chrome
@@ -35,5 +44,4 @@ export AUR=(
   visual-studio-code-bin
   z-dir-jump-git
   zoom
-  # balena-etcher
 )
