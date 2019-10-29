046667842d590a2011147e8a66217347d4cc5ae6
Lucio Charallo <hi@lucio.app>
2019-10-29 10:01:48 -0300

    install fonts with emoji

--- config/install.sh
+++ config/install.sh
@@ -9,4 +9,8 @@ echo_info "Applying custom settings..."
 # sudo sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/g' /etc/default/grub
 # sudo grub update
 
+# Change default fonts on Gnome to support emoji
+sudo cp ~/.dotfiles/config/fonts/75-noto-color-emoji.conf /etc/fonts/conf.avail/
+sudo ln -sf /etc/fonts/conf.avail/75-noto-color-emoji.conf /etc/fonts/conf.d/
+
 echo_done "Custom settings applied"
