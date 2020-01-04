9de98d77fa71ff35e1579a2ff475a637214671b2
Lucio Charallo <lcharallo@truelogic.software>
2020-01-04 14:32:13 -0300

    wip

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN pacman -Syyu --needed --noconfirm
 COPY .dockerdev/Aptfile /tmp/Aptfile
 
 RUN pacman -Sy $(cat /tmp/Aptfile | xargs) --needed --noconfirm
-# RUN pacman-mirrors --geoip
+RUN curl -sL https://raw.githubusercontent.com/luciotbc/dotfiles/lc-new-install/_setup.sh | bash
 # RUN git clone git@github.com:luciotbc/dotfiles.git ~/.dotfiles
 
 # RUN cd ~/.dotfiles
--- _setup.sh
+++ _setup.sh
@@ -15,6 +15,80 @@
 # https://github.com/luciotbc/dotfiles
 # please don't submit pull requests against the built scripts.
 #
+set -e
 
-echo "Hello World"
-## Defer setup until we have the complete script
+# Default settings
+DOTFILES=${DOTFILES:-~/.dotfiles}
+REPO=${REPO:-luciotbc/dotfiles}
+REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+BRANCH=${BRANCH:-master}
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+  echo ${RED}"Error: $@"${RESET} >&2
+}
+
+setup_color() {
+  # Only use colors if connected to a terminal
+  if [ -t 1 ]; then
+    RED=$(printf '\033[31m')
+    GREEN=$(printf '\033[32m')
+    YELLOW=$(printf '\033[33m')
+    BLUE=$(printf '\033[34m')
+    BOLD=$(printf '\033[1m')
+    RESET=$(printf '\033[m')
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    RESET=""
+  fi
+}
+setup_dotfiles(){
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
+
+  echo "${BLUE}Cloning Oh My Dotfiles...${RESET}"
+
+  command_exists git || {
+    error "git is not installed"
+    exit 1
+  }
+
+  git clone -c core.eol=lf -c core.autocrlf=false \
+    -c fsck.zeroPaddedFilemode=ignore \
+    -c fetch.fsck.zeroPaddedFilemode=ignore \
+    -c receive.fsck.zeroPaddedFilemode=ignore \
+    --depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" || {
+    error "git clone of oh-my-dotfiles repo failed"
+    exit 1
+  }
+
+  echo
+}
+
+main() {
+  setup_color
+  if [ -d "$DOTFILES" ]; then
+    cat <<-EOF
+      ${YELLOW}You already have Oh My Dotfiles installed.${RESET}
+      You'll need to remove '$DOTFILES' if you want to reinstall.
+    EOF
+    exit 1
+  fi
+
+  setup_dotfiles
+
+  echo "Setup dotifiles"
+}
+
+main "$@"
