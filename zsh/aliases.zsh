b17ffb6325764e917040438966d71afbd56c9123
Lucio Charallo <hi@lucio.app>
2020-05-12 12:57:49 -0300

    tweeaks plus script to reconnect my headset

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias :q="exit"
 
 # System
 alias sysupdate="sudo pacman -Syu --needed --noconfirm && yay -Syu --needed --noconfirm && yay -Scc --noconfirm"
+alias edot="code ~/.dotfiles"
 
 # Local postgresql
 alias pgstart="sudo systemctl start postgresql.service"
@@ -28,8 +29,18 @@ alias g2m="sudo chown -R lucio:lucio"
 # Git
 alias glogp="git log --oneline --pretty='%C(normal bold)%h%Creset - %Cblue%cn%Creset - %ad - %C(normal dim)%s%Creset' --date=short"
 
-#SRO
-alias botb="wine ~/.wine/drive_c/sro/bucktbc/sbot/SBotP_1.0.38.exe"
-alias srob="wine ~/.wine/drive_c/sro/bucktbc/Silkroad.exe"
-alias botl="wine ~/.wine/drive_c/sro/lucktbc/sbot/SBotP.exe"
-alias srol="wine ~/.wine/drive_c/sro/lucktbc/Silkroad.exe"
+# My scripts
+alias rh="~/.lscripts/reconnect-headset.sh"
+
+# My paths
+alias cdw="cd ~/.wine/drive_c"
+alias cdp="cd ~/play"
+alias cddot="cd ~/.dotfiles"
+
+# SRO
+alias botb="wine ~/.wine/drive_c/sro/bucktbc/sbot/SBotP_1.0.38.exe </dev/null &>/dev/null &"
+alias srob="wine ~/.wine/drive_c/sro/bucktbc/Silkroad.exe </dev/null &>/dev/null &"
+alias botl="wine ~/.wine/drive_c/sro/lucktbc/sbot/SBotP_1.0.38.exe </dev/null &>/dev/null &"
+alias bottl="wine ~/.wine/drive_c/sro/lucktbc/sbot_trade/SBotP_1.0.38.exe </dev/null &>/dev/null &"
+alias srol="wine ~/.wine/drive_c/sro/lucktbc/Silkroad.exe </dev/null &>/dev/null &"
+
