e08f0c061d50842a5aeb5d108c94b284b34ca668
Lucio Charallo <hi@lucio.app>
2020-05-12 13:11:53 -0300

    Lc master/reset headset2 (#22)
    * tweeaks plus script to reconnect my headset
    
    * small improvement

--- lscripts/src/reconnect-headset.sh
+++ lscripts/src/reconnect-headset.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # harded coded to WH-1000XM3
-device=00:05:16:5D:EF:E6
 address=70:26:05:E0:3E:CB
 
-echo "Disconnect ($device)"
+echo "Disconnect"
 echo -e "disconnect $address" | bluetoothctl
 sleep 5
 
-echo "Connect ($device)"
+echo "Connect"
 echo -e "connect $address" | bluetoothctl
 sleep 5
 
