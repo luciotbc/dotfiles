b27ab6ad8668319a999932ef6b9829372497d20b
Lucio Charallo <hi@lucio.app>
2020-12-29 23:17:48 -0300

    only install

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@
 . helpers.sh
 
 # Update mirrors and packages
-echo_info "Updating packages..."
-_update
+# echo_info "Updating packages..."
+# _update
 
 # # Install packages in the official repositories
 echo_info "Installing core packages..."
 _install core
 
 # Install packages in the AUR
-echo_info "Installing aur packages..."
-_install aur
+# echo_info "Installing aur packages..."
+# _install aur
 
-_symlink
+# _symlink
