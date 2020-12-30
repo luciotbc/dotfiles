f9c24adb2e71eb3174ce18302ffbd0397a8d5750
Lucio Charallo <hi@lucio.app>
2020-12-29 23:30:22 -0300

    update

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 
 # Update mirrors and packages
 # echo_info "Updating packages..."
-# _update
+_update
 
 # # Install packages in the official repositories
 echo_info "Installing core packages..."
-_install core
+# _install core
 
 # Install packages in the AUR
 # echo_info "Installing aur packages..."
